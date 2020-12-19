fbb7dccc69d699f8e1e9f7f59f85bde44ede74f5
hungngocphat01 <hungngocphat01@gmail.com>
2020-12-19 10:00:05 +0700

    Add placeholder functions

--- cachehndl_module.py
+++ cachehndl_module.py
@@ -2,3 +2,5 @@ import hashlib
 
 def md5(string):
     return hashlib.md5(string.encode("UTF-8")).digest()
+
+# Insert your functions here
---
+++ reqhndl_module.py
@@ -0,0 +1,5 @@
+import logging_module
+import cachehndl_module
+
+def request_handling(c, a):
+    # Insert your code here
