2f5ff44a95334ef10bafdaf1f6b7d1520fdbb333
hungngocphat01 <hungngocphat01@gmail.com>
2020-12-25 21:28:57 +0700

    Minor changes

--- requesthndl_module.py
+++ requesthndl_module.py
@@ -1,20 +1,25 @@
+from os import replace
 import socket
-import logging_module
+from logging_module import *
+
+BUFF_SIZE = 1024
 
 def parse_header(request_content: bytes) -> dict:
     pass
 
 def is_blocked(url: str) -> bool:
     pass
+    
 
 def get_target_info(header: dict) -> dict:
     pass
 
+
 def send_403_forbidden(client_socket: socket.socket):
     pass
 
-def recvall(socket: socket.socket) -> bytes:
+def recvall(s: socket.socket):
     pass
 
-def handle_http_request(c: socket.socket, request_content: bytes):
+def handle_http_request(c: socket.socket, a: tuple):
     pass
