4c8f9b87909ba6b877be2f7c17352b8a83d3cf7b
hungngocphat01 <hungngocphat01@gmail.com>
2021-01-01 23:00:10 +0700

    Wrapped in try-except, check for empty response

--- requesthndl_module.py
+++ requesthndl_module.py
@@ -1,6 +1,7 @@
 from os import replace
 import socket
 from logging_module import *
+import threading
 
 BUFFER_SIZE = 1024
 
@@ -36,7 +37,7 @@ def parse_header(request_content: bytes) -> dict:
     # Add other values to dict
     splitedlist = [i.split(':') for i in lst_request]
     i = 2
-    while i < len(splitedlist)-1:
+    while i < len(splitedlist):
         dict_headers.update({splitedlist[i][0]: splitedlist[i][1]})
         i = i + 1
     return dict_headers
@@ -57,53 +58,66 @@ def recvall(s: socket.socket):
 
 
 def handle_http_request(c: socket.socket, a: tuple):
-    # Receive data from client
-    request_content = recvall(c)
-    log(f"{a} Request retrieved.")
-    
-    # parse request header
-    request_headers = parse_header(request_content)
-    dict_hostPort = get_target_info(request_headers)
-    
-    # Check method whether it is POST or GET
-    method = request_headers["Method"]
-    if(method != "GET" and method != "POST"):
-        c.close()
-        return
-
-    # Check if the url is blocked or not
-    isBlocked = is_blocked(request_headers["URI"])
-    if isBlocked:
-        send_403_forbidden(c)
-        return
-    # Get host and port of the destination server
-    host = dict_hostPort["host"].strip()
-    port = dict_hostPort["port"]
-
-    # Send request data of client to destination server
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.connect((host, port))
-    server_socket.sendall(request_content)
-    # Forward server's reply to client
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    log(f"{a} Forwarding: {request_headers['URI']}")
-    with server_socket:
-        while True:
-            # Transfering 4 KB can't be slower than 1.5s in normal condition
-            server_socket.settimeout(1.5)
-            reply = b""
-            try:
-                reply = server_socket.recv(BUFFER_SIZE)
-            except socket.timeout:
-                pass
-            if not reply:
-                break
-            c.sendall(reply)
-    log(f"{a} Forwarded: {request_headers['URI']}")
-    
-    # Close connection
-    server_socket.close()
-    c.close()
+    try:
+        # Receive data from client
+        request_content = recvall(c)
 
-    log(f"{a} Connection closed.")
-    return
+        if (len(request_content) == 0):
+            raise Exception
+
+        log(f"{a} Request retrieved.", color="yellow")
+        
+        # parse request header
+        request_headers = parse_header(request_content)
+        dict_hostPort = get_target_info(request_headers)
+        
+        # Check method whether it is POST or GET
+        method = request_headers["Method"]
+        if(method != "GET" and method != "POST"):
+            log(f"{a} Method unsupported.", color="red")
+            c.close()
+            return
+
+        # Check if the url is blocked or not
+        isBlocked = is_blocked(request_headers["URI"])
+        if isBlocked:
+            send_403_forbidden(c)
+            log(f"{a} Forbidden website: {request_headers['URI']}", color="red")
+            return
+        # Get host and port of the destination server
+        host = dict_hostPort["host"].strip()
+        port = dict_hostPort["port"]
+        
+        log(f"{a} Destinataion: {host}:{port}", color="green")
+
+        # Send request data of client to destination server
+        server_socket.connect((host, port))
+        server_socket.sendall(request_content)
+        # Forward server's reply to client
+
+        log(f"{a} Forwarding: {request_headers['URI']}", color="yellow")
+        with server_socket:
+            while True:
+                # Transfering 4 KB can't be slower than 1.5s in normal condition
+                server_socket.settimeout(1.5)
+                reply = b""
+                try:
+                    reply = server_socket.recv(BUFFER_SIZE)
+                except socket.timeout:
+                    pass
+                if not reply:
+                    break
+                c.sendall(reply)
+        log(f"{a} Forwarded: {request_headers['URI']}")
+    except Exception:
+        pass
+    finally:
+        # Close connection
+        server_socket.close()
+        c.close()
+
+        log(f"{a} Connection closed.", color="red")
+        return
