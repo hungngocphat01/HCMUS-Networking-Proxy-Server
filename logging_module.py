45a5d31c112713c49f7c23ac344822217b6b7d64
hungngocphat01 <hungngocphat01@gmail.com>
2021-01-01 22:59:21 +0700

    Change color option

--- logging_module.py
+++ logging_module.py
@@ -2,12 +2,8 @@ import datetime
 from termcolor import colored
 import sys
 
-def log(message, iserror=False):
+def log(message, color="blue"):
     now = datetime.datetime.now().strftime("[%d-%m-%Y %H:%M:%S]")
-    
-    color = "blue"
-    if (iserror):
-        color = "red"
 
     buffer = f"{colored(now, color)} {message}"
     # Print log to stdout and stderr
