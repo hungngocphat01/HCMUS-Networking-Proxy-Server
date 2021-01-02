e5dfe7ea74bc8b29f1d8c8b5d3f4b3e4c2766bb1
hungngocphat01 <hungngocphat01@gmail.com>
2021-01-02 11:40:02 +0700

    Add req_log, add optional color parameter

--- logging_module.py
+++ logging_module.py
@@ -2,9 +2,21 @@ import datetime
 from termcolor import colored
 import sys
 
-def log(message, color="blue"):
+def log(message, color="blue", newline=False):
     now = datetime.datetime.now().strftime("[%d-%m-%Y %H:%M:%S]")
 
     buffer = f"{colored(now, color)} {message}"
-    # Print log to stdout and stderr
+    if (newline):
+        buffer = "\n" + buffer
+    # Print log to stdout
     print(buffer)
+
+def req_log(request_info: dict):
+    now = datetime.datetime.now().strftime("[%d-%m-%Y %H:%M:%S]")
+
+    log("Request information", color="blue")
+    log(f"Method: {request_info['Method']}", color="blue")
+    log(f"URI: {request_info['URI']}", color="blue")
+    if "Content-Type" in request_info.keys():
+        log(f"Content-Type: {request_info['Content-Type']}", color="blue")
+    print("\n")
